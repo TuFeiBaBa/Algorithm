4f899c337e7ff0eebce0c572320f05e661f7087c
wuzhanqiao <wuzhanqiao@dm-ai.com>
2022-03-27 09:49:53 +0800

    如果相邻两个颜色均相同则删除当前颜色

---
+++ leetcode/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -0,0 +1,27 @@
+/**
+* 贪心算法。看错题，不是消消乐..
+**/
+#include <string>
+using namespace std;
+class Solution {
+public:
+	bool winnerOfGame(string colors) {
+		int freq[2] = { 0, 0 };
+		char cur = 'C';
+		int cnt = 0;
+		for (char c : colors) {
+			if (c != cur) {
+				cur = c;
+				cnt = 1;
+			}
+			else if (++cnt >= 3) {
+				++freq[cur - 'A'];
+			}
+		}
+		return freq[0] > freq[1];
+	}
+};
+
+int main() {
+
+}
