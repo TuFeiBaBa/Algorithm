f6ba08d06ed0c3584c67f13089156828bdd95db8
wuzhanqiao <wuzhanqiao@dm-ai.com>
2022-04-20 11:43:33 +0800

    课程表 II：解题步骤

--- leetcode/graph/course-schedule-ii.cpp
+++ leetcode/graph/course-schedule-ii.cpp
@@ -6,8 +6,34 @@ using namespace std;
 
 /**
  * 课程表 II https://leetcode-cn.com/problems/course-schedule-ii/
+ * 解题步骤：
+ * 1.建图
+ * 2.遍历图
+ *	1.onStack判断是否有环
+ *  2.visited判断是否已经访问过，用于剪枝
+ *  3.逆后序就是拓扑排序，也是符合要求的课程安排顺序
  */
 class Solution {
+private:
+	vector<vector<int>> graph;
+	vector<int> paths;
+	vector<int> visited;
+
+	bool dfs(int course) {
+		visited[course] = 1;
+		//前序是不行的
+		//paths.push_back(course);
+		for (int c : graph[course]) {
+			if (visited[c] == 1) return false;
+			if (visited[c] == 2) continue;
+			if (!dfs(c)) return false;
+		}
+		visited[course] = 2;
+		//这里是后序，拓扑排序是逆后序。最终结果还得翻转一下。
+		paths.push_back(course);
+		return true;
+	}
+
 public:
 
 	vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
@@ -28,25 +54,6 @@ public:
 		}
 		reverse(paths.begin(), paths.end());
 		return paths;
-	}
-private:
-	vector<vector<int>> graph;
-	vector<int> paths;
-	vector<int> visited;
-
-	bool dfs(int course) {
-		visited[course] = 1;
-		//前序是不行的
-		//paths.push_back(course);
-		for (int c : graph[course]) {
-			if (visited[c] == 1) return false;
-			if (visited[c] == 2) continue;
-			if(!dfs(c)) return false;
-		}
-		visited[course] = 2;
-		//这里是后序，拓扑排序是逆后序。最终结果还得翻转一下。
-		paths.push_back(course);
-		return true;
 	}
 };
 
