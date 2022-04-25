2d304ef11eb1d777fef2c4cc8239eb86bb52e0ad
wuzhanqiao <wuzhanqiao@dm-ai.com>
2022-04-25 14:07:49 +0800

    矩阵中的最长递增路径：BFS&拓扑排序

--- leetcode/graph/longest-increasing-path-in-a-matrix.cpp
+++ leetcode/graph/longest-increasing-path-in-a-matrix.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <queue>
 #include <iostream>
 
 using namespace std;
@@ -14,37 +15,82 @@ using namespace std;
  *	1.matrix[x][y] <= matrix[nx][ny]
  */
 class Solution {
-private:
-	int dfs(int x, int y, vector<vector<int>>& matrix, vector<vector<int>>& dp, vector<vector<int>>& move) {
-		//if (x < 0 || y < 0 || x >= matrix.size() || y >= matrix[0].size()) return 0;
-		//剪枝
-		if (dp[x][y] != -1) return dp[x][y];
-		//默认是1
-		int maxPath = 1;
-		for (int i = 0; i < move.size(); i++) {
-			int nx = x + move[i][0];
-			int ny = y + move[i][1];
-			if (nx < 0 || ny < 0 || nx >= matrix.size() || ny >= matrix[0].size()) continue;
-			if (matrix[x][y] <= matrix[nx][ny]) continue;
-			maxPath = max(maxPath, dfs(nx, ny, matrix, dp, move) + 1);
-		}
-		dp[x][y] = maxPath;
-		return maxPath;
-	}
+	//private:
+	//	int dfs(int x, int y, vector<vector<int>>& matrix, vector<vector<int>>& dp, vector<vector<int>>& move) {
+	//		//if (x < 0 || y < 0 || x >= matrix.size() || y >= matrix[0].size()) return 0;
+	//		//剪枝
+	//		if (dp[x][y] != -1) return dp[x][y];
+	//		//默认是1
+	//		int maxPath = 1;
+	//		for (int i = 0; i < move.size(); i++) {
+	//			int nx = x + move[i][0];
+	//			int ny = y + move[i][1];
+	//			if (nx < 0 || ny < 0 || nx >= matrix.size() || ny >= matrix[0].size()) continue;
+	//			if (matrix[x][y] <= matrix[nx][ny]) continue;
+	//			maxPath = max(maxPath, dfs(nx, ny, matrix, dp, move) + 1);
+	//		}
+	//		dp[x][y] = maxPath;
+	//		return maxPath;
+	//	}
+	//
+	//public:
+	//	int longestIncreasingPath(vector<vector<int>>& matrix) {
+	//		vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), -1));
+	//		vector<vector<int>> move{ {0,1},{1,0},{0,-1},{-1,0} };
+	//		int maxPath = 0;
+	//		for (int i = 0; i < matrix.size(); i++) {
+	//			for (int j = 0; j < matrix[0].size(); j++) {
+	//				int dist = dfs(i, j, matrix, dp, move);
+	//				maxPath = max(maxPath, dist);
+	//				//dp[i][j] = dist;
+	//			}
+	//		}
+	//		return maxPath;
+	//	}
 
 public:
 	int longestIncreasingPath(vector<vector<int>>& matrix) {
-		vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), -1));
+		//vector<vector<int>> in(matrix.size(), vector<int>(matrix[0].size()));
+		vector<vector<int>> out(matrix.size(), vector<int>(matrix[0].size()));
 		vector<vector<int>> move{ {0,1},{1,0},{0,-1},{-1,0} };
-		int maxPath = 0;
 		for (int i = 0; i < matrix.size(); i++) {
 			for (int j = 0; j < matrix[0].size(); j++) {
-				int dist = dfs(i, j, matrix, dp, move);
-				maxPath = max(maxPath, dist);
-				//dp[i][j] = dist;
+				for (int k = 0; k < move.size(); k++) {
+					int nx = i + move[k][0];
+					int ny = j + move[k][1];
+					if (nx >= 0 && ny >= 0 && nx < matrix.size() && ny < matrix[0].size() && matrix[i][j] < matrix[nx][ny]) {
+						out[i][j]++;
+					}
+				}
 			}
 		}
-		return maxPath;
+
+		queue<pair<int, int>> q;
+		for (int i = 0; i < matrix.size(); i++) {
+			for (int j = 0; j < matrix[0].size(); j++) {
+				if (out[i][j] == 0) q.emplace(i, j);
+			}
+		}
+		int deep = 0;
+		while (!q.empty()) {
+			deep++;
+			int size = q.size();
+			for (int m = 0; m < size; m++) {
+				pair<int, int> pos = q.front();
+				q.pop();
+				int i = pos.first;
+				int j = pos.second;
+				for (int k = 0; k < move.size(); k++) {
+					int nx = i + move[k][0];
+					int ny = j + move[k][1];
+					if (nx >= 0 && ny >= 0 && nx < matrix.size() && ny < matrix[0].size() && matrix[i][j] > matrix[nx][ny]) {
+						out[nx][ny]--;
+						if (out[nx][ny] == 0) q.emplace(nx, ny);
+					}
+				}
+			}
+		}
+		return deep;
 	}
 };
 
