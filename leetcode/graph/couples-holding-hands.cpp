392c296c13abbfe01210403f8ed36be68d2d9437
wuzhanqiao <wuzhanqiao@dm-ai.com>
2022-05-26 15:04:22 +0800

    情侣牵手

--- leetcode/graph/couples-holding-hands.cpp
+++ leetcode/graph/couples-holding-hands.cpp
@@ -14,6 +14,12 @@ using namespace std;
 *
 * 贪心：（证明可参考：https://leetcode.cn/problems/couples-holding-hands/solution/liang-chong-100-de-jie-fa-bing-cha-ji-ta-26a6/)
 *	贪心证明的思路：分析各种局部情况，然后从局部推到全局
+*		1.处理到k位置时，还剩n对情侣没有处理。k位置的两个人与k + x位置的4个人，是两队交错的情侣。交换一次，就只剩n - 2对情侣没有处理。
+*		2.处理到k位置时，还剩n对情侣没有处理。k位置的两个人与k + x位置的4个人，k位置的其中一人和k + x位置的一人是一队情侣。剩下两人，不是情侣。
+*		  则交换一次后，只剩n - 1对情侣没有处理。
+*		3.注意1、2点，无论k 、k + x的情况如何，处理完k位置后，再处理k + 1位置，肯定不会影响k位置。而按1、2点为原则处理，显然可以完整处理后序
+*		  所有的错误情侣对。
+*		4.还有一个小问题：如何快速确定x在哪里。
 *
 * 广度优先搜索：https://leetcode.cn/problems/couples-holding-hands/solution/qing-lu-qian-shou-by-leetcode-solution-bvzr/
 * 核心思路：将不匹配的row[i]、row[i + 1]，视为无向图上的连通的两个节点。然后用BFS遍历图。（可能是多个连通图组成的图，所以要遍历每个可能的起点，进行BFS）
