e9222db1d210633ed5707e4097a4623b7734812c
wuzhanqiao <wuzhanqiao@dm-ai.com>
2022-05-26 14:42:59 +0800

    情侣牵手

--- leetcode/graph/couples-holding-hands.cpp
+++ leetcode/graph/couples-holding-hands.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 /**
 * 情侣牵手：https://leetcode.cn/problems/couples-holding-hands/
-* 
-* 对于 row[i]，其男/女朋友为 row[i] ^ 1 (异或运算) (注意：好处是无论当前的值是奇数偶数，即数值二进制右端是0或1。而普通的+、-，
-* 需要先判断当前是奇数偶数，再来决定+、-1)
-* 
+*
+* 对于 row[i]，其男/女朋友为 row[i] ^ 1 (异或运算，其实只是改变了row[i]二进制右端的取值)
+* (注意：好处是无论当前的值是奇数偶数，即数值二进制右端是0或1。而普通的+、-，需要先判断当前是奇数偶数，再来决定+、-1)
+*
 * 并查集：https://leetcode.cn/problems/couples-holding-hands/solution/qing-lu-qian-shou-by-leetcode-gl1c/
 * 核心思路；如果我们有 k 对情侣形成了错误环，需要交换 k - 1 次才能让情侣牵手。
 * 于是问题转化成 n / 2 对情侣中，有多少个这样的环。
-* 
+*
 * 贪心：（证明可参考：https://leetcode.cn/problems/couples-holding-hands/solution/liang-chong-100-de-jie-fa-bing-cha-ji-ta-26a6/)
 *	贪心证明的思路：分析各种局部情况，然后从局部推到全局
-* 
+*
 * 广度优先搜索：https://leetcode.cn/problems/couples-holding-hands/solution/qing-lu-qian-shou-by-leetcode-solution-bvzr/
 * 核心思路：将不匹配的row[i]、row[i + 1]，视为无向图上的连通的两个节点。然后用BFS遍历图。（可能是多个连通图组成的图，所以要遍历每个可能的起点，进行BFS）
 */
@@ -80,5 +80,5 @@ int main() {
 	vector<int> row{ 5,4,2,6,3,1,0,7 };
 	int ret = su.minSwapsCouples(row);
 	cout << ret << endl;
-	
+
 }
