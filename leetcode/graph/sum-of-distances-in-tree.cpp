5aabc077ea24f23f36b4e0e36086ceada5d3ae49
wuzhanqiao <wuzhanqiao@dm-ai.com>
2022-05-28 10:21:47 +0800

    树中距离之和

--- leetcode/graph/sum-of-distances-in-tree.cpp
+++ leetcode/graph/sum-of-distances-in-tree.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 /**
 * 树中距离之和：https://leetcode.cn/problems/sum-of-distances-in-tree/
-* 
+*
 * 不完善的思路：
 *	1.1 <= n <= 3 * 10^4。暴力解，获取一个节点的距离和，就要以该节点为根节点，遍历整颗树。遍历整颗树的时间是n。
 *	  但是获取所有节点的距离，时间复杂度，就上升至O(n^2)了。
@@ -10,7 +10,7 @@ using namespace std;
 *	3.想一想，在遍历一颗树的时候，我们能获得什么？树的高度、前中后序、树的深度、【左子树、右子树大小，或者说节点数目】
 *	4.（错误：没有注意到，如示例一，这不是个二叉树，而是N叉树）知道左右子树节点数目，当将根节点，移动到左节点时，
 *	  左节点的距离和，必然要减少（左节点的子树节点数目 - 1）(距离拉近了)，加上（右节点的子树节点数目 + 1）(包括原根节点)
-* 
+*
 *	  正确思路：定义dp[u]表示以u为根的子树，它的所有子节点到它的距离之和，同时定义sz[u]表示以u为根的子树的节点数量（包括了u）。
 *	  则			dp[u] = dp[v1] + sz[v1] + dp[v2] + sz[v2] + ...  (v1、v2是u的直接子节点)
 *					sz[u] = sz[v1] + sz[v2]
@@ -26,9 +26,53 @@ using namespace std;
 *											sz[v] = sz[v] + sz[u]
 */
 class Solution {
+private:
+	vector<int> dp;
+	vector<int> sz;
+	vector<int> ans;
+	void dfs(vector<vector<int>>& graph, int parent) {
+		sz[parent] = 1;
+		for (int e : graph[parent]) {
+			if (sz[e] != 0) continue;
+			dfs(graph, e);
+			sz[parent] += sz[e];
+			dp[parent] += dp[e] + sz[e];
+		}
+	}
+
+	void dfs2(vector<vector<int>>& graph, int old, int n) {
+		dp[old] = dp[old] - (dp[n] + sz[n]);
+		sz[old] = sz[old] - sz[n];
+		dp[n] = dp[n] + (dp[old] + sz[old]);
+		sz[n] = sz[n] + sz[old];
+		ans[n] = dp[n];
+		for (int e : graph[n]) {
+			if (e == old) continue;
+			dfs2(graph, n, e);
+		}
+		dp[n] = dp[n] - (dp[old] + sz[old]);
+		sz[n] = sz[n] - sz[old];
+		dp[old] = dp[old] + (dp[n] + sz[n]);
+		sz[old] = sz[old] + sz[n];
+	}
 public:
 	vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+		vector<vector<int>> graph(n);
+		dp = vector<int>(n);
+		sz = vector<int>(n);
+		ans = vector<int>(n);
+		for (int i = 0; i < edges.size(); i++) {
+			graph[edges[i][0]].push_back(edges[i][1]);
+			graph[edges[i][1]].push_back(edges[i][0]);
+		}
 
+		dfs(graph, 0);
+		ans[0] = dp[0];
+		for (int e : graph[0]) {
+			dfs2(graph, 0, e);
+		}
+
+		return ans;
 	}
 };
 
