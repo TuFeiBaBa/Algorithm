42ed93991c951252e04491d8af659965afac9a7e
wuzhanqiao <wuzhanqiao@dm-ai.com>
2022-06-04 10:53:25 +0800

    由斜杠划分区域

---
+++ leetcode/graph/regions-cut-by-slashes.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <string>
+using namespace std;
+/*
+* 由斜杠划分区域：https://leetcode.cn/problems/regions-cut-by-slashes/
+* 题解一：https://leetcode.cn/problems/regions-cut-by-slashes/solution/you-xie-gang-hua-fen-qu-yu-by-leetcode-67xb/
+* 题解二（转成求岛屿个数。并查集代码在评论区。比解法一更耗时，但思路更简单）：https://leetcode.cn/problems/regions-cut-by-slashes/solution/c-dong-hua-zhuan-huan-cheng-dao-yu-ge-sh-guve/
+* 
+* 核心思想：题目其实是考核连通性。考连通性，在目前已掌握的算法里面，应该优先选择并查集。
+*/
+class Solution {
+public:
+	int regionsBySlashes(vector<string>& grid) {
+
+	}
+};
+
+int main() {
+
+}
