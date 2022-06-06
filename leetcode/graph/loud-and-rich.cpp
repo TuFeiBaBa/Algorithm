f44bad87d52d472369961f8013eb456531ad462e
wuzhanqiao <wuzhanqiao@dm-ai.com>
2022-06-06 18:40:55 +0800

    喧闹和富有

---
+++ leetcode/graph/loud-and-rich.cpp
@@ -0,0 +1,20 @@
+﻿#include <vector>
+
+using namespace std;
+
+/*
+* 喧闹和富有：https://leetcode.cn/problems/loud-and-rich/
+* 
+* "answer[x] = y 的前提是，在所有拥有的钱肯定不少于 person x 的人中，person y 是最安静的人（也就是安静值 quiet[y] 最小的人）"
+* 
+* 不少于，即可以是等于，即可以是persion x自己。
+*/
+class Solution {
+public:
+	vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+
+	}
+};
+int main() {
+
+}
