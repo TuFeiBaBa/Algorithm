7f25b9c773204d5a20b9957aa948d2c0fdac72c6
wuzhanqiao <wuzhanqiao@dm-ai.com>
2022-06-15 10:13:14 +0800

    最长公共前缀

---
+++ leetcode/string/longest-common-prefix.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+/*
+* 最长公共前缀：https://leetcode.cn/problems/longest-common-prefix/
+*
+* 核心思路：逐列对比即可，一旦存在不一致，前面对比成功的列就是最长公共前缀
+*
+* 时间复杂度：O(mn)。m是字符串数量，n是公共前缀的长度。最坏情况下，是所有字符串都相等。
+*/
+class Solution {
+public:
+	string longestCommonPrefix(vector<string>& strs) {
+
+	}
+};
+
+int main() {
+
+}
