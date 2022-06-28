a4a1590410627e42d706597488bdd26e95ce5eb7
wuzhanqiao <wuzhanqiao@dm-ai.com>
2022-06-28 09:58:53 +0800

    阈值距离内邻居最少的城市

---
+++ leetcode/graph/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+using namespace std;
+/*
+* 阈值距离内邻居最少的城市：https://leetcode.cn/problems/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/
+* 
+* 用BFS应该也是能AC的，从数据量上来看：2 <= n <= 100，1 <= edges.length <= n * (n - 1) / 2
+* 
+* BFS的时间复杂度是O(V + E)V，计算量最大不会超过100w。
+* 
+* 最优解是Floyd算法。后续有时间再学习。
+*/
+class Solution {
+public:
+	int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+
+	}
+};
+
+int main() {
+
+}
