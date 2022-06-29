92b522cc4dfab04f4448bff64447c224e4a25e5e
wuzhanqiao <wuzhanqiao@dm-ai.com>
2022-06-29 11:32:29 +0800

    有效括号

---
+++ leetcode/string/valid-parentheses.cpp
@@ -0,0 +1,19 @@
+#include<string>
+using namespace std;
+/*
+* 有效括号：https://leetcode.cn/problems/valid-parentheses/
+* 
+* 这道题更像是在考怎么把代码写得更优雅
+* 
+* 标准思路是：栈。用map存储对应关系。然后将key入栈。碰到value时就弹栈。
+*/
+class Solution {
+public:
+	bool isValid(string s) {
+
+	}
+};
+
+int main() {
+
+}
