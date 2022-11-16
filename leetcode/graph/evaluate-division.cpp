3a11fbcd940eb0a3eaad7a54662a591b1f7156ad
qiao <1024522245@qq.com>
2022-11-16 09:42:13 +0800

    除法求值

--- leetcode/graph/evaluate-division.cpp
+++ leetcode/graph/evaluate-division.cpp
@@ -104,57 +104,106 @@ private:
 	vector<double> weight;
 };
 
+//class Solution {
+//public:
+//	vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+//		unordered_map<string, int> map;
+//		//equations.size() * 2 是为了确保数组容量足够大
+//		UnionFind uf(equations.size() * 2);
+//		int id = 0;
+//		for (int i = 0; i < equations.size(); i++) {
+//			vector<string>& equat = equations[i];
+//			string& s1 = equat[0];
+//			string& s2 = equat[1];
+//			if (map.count(s1) == 0) {
+//				map[s1] = id;
+//				id++;
+//			}
+//			if (map.count(s2) == 0) {
+//				map[s2] = id;
+//				id++;
+//			}
+//			uf.unionFun(map[s1], map[s2], values[i]);
+//		}
+//
+//		vector<double> result;
+//		for (int i = 0; i < queries.size(); i++) {
+//			vector<string>& query = queries[i];
+//			string& s1 = query[0];
+//			string& s2 = query[1];
+//			if (map.count(s1) == 0 || map.count(s2) == 0) {
+//				result.emplace_back(-1);
+//				continue;
+//			}
+//			result.emplace_back(uf.isConnect(map[s1], map[s2]));
+//		}
+//		return result;
+//	}
+//};
+
+/*
+* 除法求值 https://leetcode.cn/problems/evaluate-division/description/
+* 
+* 并查集解法：https://leetcode.cn/problems/evaluate-division/solutions/548634/399-chu-fa-qiu-zhi-nan-du-zhong-deng-286-w45d/
+* floyd解法：https://leetcode.cn/problems/evaluate-division/solutions/548585/chu-fa-qiu-zhi-by-leetcode-solution-8nxb/
+*/
 class Solution {
 public:
 	vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-		unordered_map<string, int> map;
-		//equations.size() * 2 是为了确保数组容量足够大
-		UnionFind uf(equations.size() * 2);
-		int id = 0;
+		unordered_map<string, int> idx;
+		int n = 0;
 		for (int i = 0; i < equations.size(); i++) {
-			vector<string>& equat = equations[i];
-			string& s1 = equat[0];
-			string& s2 = equat[1];
-			if (map.count(s1) == 0) {
-				map[s1] = id;
-				id++;
-			}
-			if (map.count(s2) == 0) {
-				map[s2] = id;
-				id++;
-			}
-			uf.unionFun(map[s1], map[s2], values[i]);
+			//用map来建图的一种思路，先为string设置对应的索引
+			if (idx.find(equations[i][0]) == idx.end()) idx[equations[i][0]] = n++;
+			if (idx.find(equations[i][1]) == idx.end()) idx[equations[i][1]] = n++;
 		}
-
-		vector<double> result;
+		vector<vector<double>> g(n, vector<double>(n, -1));
+		for (int i = 0; i < equations.size(); i++) {
+			int x = idx[equations[i][0]];
+			int y = idx[equations[i][1]];
+			g[x][y] = values[i];
+			//无向图
+			g[y][x] = 1 / values[i];
+		}
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					//写法错误：g[i][k] 不等价于g[i][k] > 0
+					//g[i][k] < 0时，也是true
+					//if(g[i][k]&& g[k][j])
+					if (g[i][k] > 0 && g[k][j] > 0) {
+						g[i][j] = g[i][k] * g[k][j];
+					}
+				}
+			}
+		}
+		vector<double> ans(queries.size(), -1);
 		for (int i = 0; i < queries.size(); i++) {
-			vector<string>& query = queries[i];
-			string& s1 = query[0];
-			string& s2 = query[1];
-			if (map.count(s1) == 0 || map.count(s2) == 0) {
-				result.emplace_back(-1);
-				continue;
+			int x = idx.find(queries[i][0]) == idx.end() ? -1 : idx[queries[i][0]];
+			int y = idx.find(queries[i][1]) == idx.end() ? -1 : idx[queries[i][1]];
+			if (x >= 0 && y >= 0) {
+				ans[i] = g[x][y];
 			}
-			result.emplace_back(uf.isConnect(map[s1], map[s2]));
 		}
-		return result;
+		return ans;
 	}
 };
 
 int main() {
 	Solution su;
-	/*vector<vector<string>> equations{ {"a","b"},{"b","c" } };
+	vector<vector<string>> equations{ {"a","b"},{"b","c" } };
 	vector<double> values{ 2.0,3.0 };
-	vector<vector<string>> quueries{ {"a","c"},{"b","a"},{"a","e"},{"a","a"},{"x","x"} };*/
+	vector<vector<string>> quueries{ {"a","c"},{"b","a"},{"a","e"},{"a","a"},{"x","x"} };
 
-	vector<vector<string>> equations{ {"a", "b"} ,{"b", "c"} };
+	/*vector<vector<string>> equations{ {"a", "b"} ,{"b", "c"} };
 	vector<double> values{ 1.5, 2.5 };
-	vector<vector<string>> quueries{ {"a","c"}, {"c","b"} };
+	vector<vector<string>> quueries{ {"a","c"}, {"c","b"} };*/
 	vector<double> res = su.calcEquation(equations, values, quueries);
 
 	/*vector<vector<string>> equations{ {"x1", "x2"} ,{"x2", "x3"},{"x3", "x4"},{"x4", "x5" } };
 	vector<double> values{ 3.0, 4.0, 5.0, 6.0 };
 	vector<vector<string>> quueries{ {"x1","x5"} };
 	vector<double> res = su.calcEquation(equations, values, quueries);*/
+	if (-1) cout << "123" << endl;
 	for_each(res.begin(), res.end(), [](const double i) {cout << i << " "; });
 }
