5c7b4066c76d93dca0569cc789c1a3c95fa7d2c6
qiao <1024522245@qq.com>
2022-11-16 10:33:14 +0800

    课程表 II

--- leetcode/graph/course-schedule-ii.cpp
+++ leetcode/graph/course-schedule-ii.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <algorithm>
 #include <xutility>
+#include <queue>
 using namespace std;
 
 /**
@@ -13,47 +14,76 @@ using namespace std;
  *  2.visited判断是否已经访问过，用于剪枝
  *  3.逆后序就是拓扑排序，也是符合要求的课程安排顺序
  */
+//class Solution {
+//private:
+//	vector<vector<int>> graph;
+//	vector<int> paths;
+//	vector<int> visited;
+//
+//	bool dfs(int course) {
+//		visited[course] = 1;
+//		//前序是不行的
+//		//paths.push_back(course);
+//		for (int c : graph[course]) {
+//			if (visited[c] == 1) return false;
+//			if (visited[c] == 2) continue;
+//			if (!dfs(c)) return false;
+//		}
+//		visited[course] = 2;
+//		//这里是后序，拓扑排序是逆后序。最终结果还得翻转一下。
+//		paths.push_back(course);
+//		return true;
+//	}
+//
+//public:
+//
+//	vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+//		graph = vector<vector<int>>(numCourses);
+//		//paths = vector<int>(numCourses)是错误的。不要当Java来写..
+//		paths = vector<int>();
+//		visited = vector<int>(numCourses);
+//		for (int i = 0; i < prerequisites.size(); i++) {
+//			graph[prerequisites[i][1]].push_back(prerequisites[i][0]);
+//		}
+//
+//		for (int i = 0; i < numCourses; i++) {
+//			//if(visited[i])也行：visited[i]为1时，dfs肯定已经发现环，退出遍历了。
+//			if (visited[i] == 2) continue;
+//			if (!dfs(i)) {
+//				return vector<int>();
+//			}
+//		}
+//		reverse(paths.begin(), paths.end());
+//		return paths;
+//	}
+//};
+
 class Solution {
-private:
-	vector<vector<int>> graph;
-	vector<int> paths;
-	vector<int> visited;
-
-	bool dfs(int course) {
-		visited[course] = 1;
-		//前序是不行的
-		//paths.push_back(course);
-		for (int c : graph[course]) {
-			if (visited[c] == 1) return false;
-			if (visited[c] == 2) continue;
-			if (!dfs(c)) return false;
-		}
-		visited[course] = 2;
-		//这里是后序，拓扑排序是逆后序。最终结果还得翻转一下。
-		paths.push_back(course);
-		return true;
-	}
-
 public:
-
 	vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-		graph = vector<vector<int>>(numCourses);
-		//paths = vector<int>(numCourses)是错误的。不要当Java来写..
-		paths = vector<int>();
-		visited = vector<int>(numCourses);
-		for (int i = 0; i < prerequisites.size(); i++) {
-			graph[prerequisites[i][1]].push_back(prerequisites[i][0]);
+		vector<vector<int>> g(numCourses);
+		vector<int> in(numCourses);
+		for (auto& e : prerequisites) {
+			g[e[1]].push_back(e[0]);
+			in[e[0]]++;
 		}
-
+		queue<int> q;
+		vector<int> ans;
 		for (int i = 0; i < numCourses; i++) {
-			//if(visited[i])也行：visited[i]为1时，dfs肯定已经发现环，退出遍历了。
-			if (visited[i] == 2) continue;
-			if (!dfs(i)) {
-				return vector<int>();
+			if (!in[i]) q.push(i);
+		}
+		while (!q.empty()) {
+			int begin = q.front();
+			q.pop();
+			ans.push_back(begin);
+			for (int end : g[begin]) {
+				in[end]--;
+				if (!in[end]) {
+					q.push(end);
+				}
 			}
 		}
-		reverse(paths.begin(), paths.end());
-		return paths;
+		return ans.size() == numCourses ? ans : vector<int>();
 	}
 };
 
