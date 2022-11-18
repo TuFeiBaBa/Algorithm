6662b37dc99bee13638e6e08e35b26e8a0d0c2ed
qiao <1024522245@qq.com>
2022-11-18 09:25:46 +0800

    最长回文子串

--- leetcode/string/longest-palindromic-substring.cpp
+++ leetcode/string/longest-palindromic-substring.cpp
@@ -46,44 +46,120 @@ using namespace std;
 *
 * 最优解是Manacher算法：https://leetcode.cn/problems/longest-palindromic-substring/solution/zui-chang-hui-wen-zi-chuan-by-leetcode-solution/
 */
-class Solution {
-private:
-	int findLongest(string& s, int start, int end) {
-		int len = 0;
-		while (start >= 0 && end < s.size()) {
-			if (s[start] != s[end]) break;
-			len = end - start + 1;
-			start--;
-			end++;
-		}
-		return len;
-	}
+//class Solution {
+//private:
+//	int findLongest(string& s, int start, int end) {
+//		int len = 0;
+//		while (start >= 0 && end < s.size()) {
+//			if (s[start] != s[end]) break;
+//			len = end - start + 1;
+//			start--;
+//			end++;
+//		}
+//		return len;
+//	}
+//
+//public:
+//	string longestPalindrome(string s) {
+//		int len = 0, start = 0, end = 0;
+//		for (int i = 0; i < s.size(); i++) {
+//			int l1 = findLongest(s, i, i);
+//			int l2 = findLongest(s, i, i + 1);
+//			if (len >= l1 && len >= l2) continue;
+//			if (l1 > l2) {
+//				len = l1;
+//				start = i - len / 2;
+//				end = i + len / 2;
+//			}
+//			else {
+//				len = l2;
+//				//其实可以在findLongest直接返回{start,end}，更方便
+//				//推导公式：
+//				//(i - start + 1) * 2 = len;
+//				start = i - len / 2 + 1;
+//				//(end - (i + 1) + 1) * 2 = len;
+//				end = len / 2 + i;
+//			}
+//		}
+//		//参数1是起始位置，参数2是子串的字符总数
+//		return s.substr(start, end - start + 1);
+//	}
+//};
 
+//class Solution {
+//private:
+//    pair<int, int> find(string& s, int start, int end) {
+//        while (start >= 0 && end < s.size() && s[start] == s[end]) {
+//            start--;
+//            end++;
+//        }
+//        return { start + 1,end - 1 };
+//    }
+//public:
+//    string longestPalindrome(string s) {
+//        int start = 0, end = 0;
+//        for (int i = 0; i < s.size(); i++) {
+//            auto p1 = find(s, i, i);
+//            auto p2 = find(s, i, i + 1);
+//            if (p1.second - p1.first > end - start) {
+//                start = p1.first;
+//                end = p1.second;
+//            }
+//            if (p2.second - p2.first > end - start) {
+//                start = p2.first;
+//                end = p2.second;
+//            }
+//        }
+//        return s.substr(start, end - start + 1);
+//    }
+//};
+
+//class Solution {
+//private:
+//    pair<int, int> find(string& s, int start, int end) {
+//        while (start >= 0 && end < s.size() && s[start] == s[end]) {
+//            start--;
+//            end++;
+//        }
+//        return { start + 1,end - 1 };
+//    }
+//public:
+//    string longestPalindrome(string s) {
+//        int start = 0, end = 0;
+//        for (int i = 0; i < s.size(); i++) {
+//            auto p1 = find(s, i, i);
+//            auto p2 = find(s, i, i + 1);
+//            if (p1.second - p1.first > end - start) {
+//                start = p1.first;
+//                end = p1.second;
+//            }
+//            if (p2.second - p2.first > end - start) {
+//                start = p2.first;
+//                end = p2.second;
+//            }
+//        }
+//        return s.substr(start, end - start + 1);
+//    }
+//};
+
+class Solution {
 public:
-	string longestPalindrome(string s) {
-		int len = 0, start = 0, end = 0;
-		for (int i = 0; i < s.size(); i++) {
-			int l1 = findLongest(s, i, i);
-			int l2 = findLongest(s, i, i + 1);
-			if (len >= l1 && len >= l2) continue;
-			if (l1 > l2) {
-				len = l1;
-				start = i - len / 2;
-				end = i + len / 2;
-			}
-			else {
-				len = l2;
-				//其实可以在findLongest直接返回{start,end}，更方便
-				//推导公式：
-				//(i - start + 1) * 2 = len;
-				start = i - len / 2 + 1;
-				//(end - (i + 1) + 1) * 2 = len;
-				end = len / 2 + i;
-			}
-		}
-		//参数1是起始位置，参数2是子串的字符总数
-		return s.substr(start, end - start + 1);
-	}
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if (n <= 1) return s;
+        int start = 0, end = 0;
+        vector<vector<bool>> dp(n, vector<bool>(n, true));
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = (dp[i + 1][j - 1]) && (s[i] == s[j]);
+                if (dp[i][j] && (j - i > end - start)) {
+                    start = i;
+                    end = j;
+                }
+            }
+        }
+        return s.substr(start, end - start + 1);
+    }
 };
 
 int main() {
