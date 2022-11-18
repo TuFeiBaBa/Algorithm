48bef263cfd45d0f89f0df3961f8f0073e1cf765
qiao <1024522245@qq.com>
2022-11-18 18:33:56 +0800

    最长公共前缀

--- leetcode/string/longest-common-prefix.cpp
+++ leetcode/string/longest-common-prefix.cpp
@@ -11,9 +11,25 @@ using namespace std;
 */
 class Solution {
 public:
-	string longestCommonPrefix(vector<string>& strs) {
-
-	}
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 1) return strs[0];
+        string ans;
+        //换成int len，min方法报错
+        size_t len = INT_MAX;
+        //可以不遍历，先拿第一个的size，遍历比较过程中，发现超出了某个字符串的长度，直接return结果
+        for (auto& s : strs) {
+            len = min(len, s.size());
+        }
+        for (int i = 0; i < len; i++) {
+            char c = strs[0][i];
+            for (int j = 1; j < strs.size(); j++) {
+                if (strs[j][i] != c) return ans;
+            }
+            //不拼接更好一点，直接记录长度，最后再切割出对应的前缀？
+            ans += c;
+        }
+        return ans;
+    }
 };
 
 int main() {
