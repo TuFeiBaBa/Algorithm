945e2058ea3d7f5989dd8fe53be3efd733498927
qiao <1024522245@qq.com>
2022-11-19 21:58:47 +0800

    有效括号

--- leetcode/string/valid-parentheses.cpp
+++ leetcode/string/valid-parentheses.cpp
@@ -1,4 +1,6 @@
 #include<string>
+#include<unordered_map>
+#include<stack>
 using namespace std;
 /*
 * 有效括号：https://leetcode.cn/problems/valid-parentheses/
@@ -8,9 +10,29 @@ using namespace std;
 * 标准思路是：栈。用map存储对应关系。然后将key入栈。碰到value时就弹栈。
 */
 class Solution {
+private:
+	unordered_map<char, char> m = {
+		{')','('},
+		{'}','{'},
+		{']','['},
+	};
 public:
 	bool isValid(string s) {
-
+		stack<char> st;
+		for (char c : s) {
+			if (m.find(c) == m.end()) {
+				st.push(c);
+			}
+			else {
+				//要判断栈是否为空，处理: ']'
+				if (st.empty() || m[c] != st.top()) {
+					return false;
+				}
+				st.pop();
+			}
+		}
+		//处理类似的情况: '[('
+		return st.empty();
 	}
 };
 
