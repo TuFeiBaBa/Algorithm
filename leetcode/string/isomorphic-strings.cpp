cf2d04860676b7c83c2f59d09e73b603e1df1b04
qiao <1024522245@qq.com>
2022-11-20 11:17:26 +0800

    同构字符串

---
+++ leetcode/string/isomorphic-strings.cpp
@@ -0,0 +1,29 @@
+#include <string>
+
+using namespace std;
+/*
+* Í¬¹¹×Ö·û´® https://leetcode.cn/problems/isomorphic-strings/description/
+*/
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> m;
+        unordered_map<char, char> r;
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            char d = t[i];
+            if (m.find(c) == m.end() && r.find(d) == r.end()) {
+                m[c] = d;
+                r[d] = c;
+            }
+            else if (m[c] != d || r[d] != c) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+
+}
